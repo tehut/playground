dcd7c4a814777bb33b18eb9a7497ec30f635e349
Tehut <tehut.m.g@gmail.com>
2017-11-08 02:41:06 -0800

    init show generate stubs serving

--- main.go
+++ main.go
@@ -7,24 +7,34 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
-	"golang.org/x/time/rate"
 )
 
-var limiter *rate.Limiter
+// var limiter *rate.Limiter
 
 func main() {
-	limiter = rate.NewLimiter(config.RateLimit, config.RateLimitBurst)
+	// limiter = rate.NewLimiter(config.RateLimit, config.RateLimitBurst)
 	router := mux.NewRouter().StrictSlash(true)
 	router.HandleFunc("/", index)
-	router.HandleFunc("/init", index)
-	router.HandleFunc("/show", index)
-	router.HandleFunc("/generate", index)
+	router.HandleFunc("/init", ksInit)
+	router.HandleFunc("/show", ksShow)
+	router.HandleFunc("/generate", ksGenerate)
 	router.HandleFunc("/depget", index)
 	router.HandleFunc("/env", index)
-
+	log.Printf("Serving on :8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+func ksInit(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "stubs/init.json")
+}
+
+func ksShow(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "stubs/show.json")
+}
+func ksGenerate(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "stubs/generate.json")
 }
---
+++ stubs/file_reader.rb
@@ -0,0 +1,97 @@
+require 'json'
+def readhash(hash)
+  hash.each do |key, value|
+    if key == "full_path"
+    value = get_file_as_string(value)
+    end
+  end
+  return hash
+end
+def get_file_as_string(filename)
+  # filename = "/Users/tehut/test/" + filename
+  data = ''
+  f = File.open(filename, "r")
+  f.each_line do |line|
+    data += line
+  end
+  return data
+end
+
+hash = File.read(ARGV[0].to_s)
+hash = JSON.parse(hash)
+
+readhash(hash)
+# class FileTree
+
+#   attr_accessor :hash
+
+#   def initialize(path)
+#     @hash = make_hash(path)
+#   end
+
+#   def to_json
+#     @hash.to_json
+#   end
+
+#   def to_html(hash=@hash, name='root')
+#     html = ""
+#     html << "<ul>"
+#     html << "<li>" << name << "</li>"
+#     if hash
+#       hash.each do |key, val|
+#         if !val
+#           html << "<li>" << key << "</li>"
+#         else
+#           html << "<li>" << self.to_html(val["children"], key) << "</li>"
+#         end
+#       end
+#     end
+#     html << "</ul>"
+#     html
+#   end
+
+#   def to_s
+#     @hash.to_s
+#   end
+
+#   def to_h
+#     @hash
+#   end
+
+
+#   private
+
+#   def make_hash(path)
+#     hash = {}
+#     Dir.entries(path).select { |file|
+#       !['.', '..'].include? file
+#     }.each do |file|
+#       full_path = File.join(path, file)
+#       # if file !=  "974dd0a850880a1fd5ea088253e44f134876f2fe.yaml"
+#         if File.directory? full_path
+#           hash[file] = {
+#             "children" => make_hash(full_path),
+#             "type" => 'directory',
+#             "full_path" => full_path
+#           }
+#         else
+#           hash[file] = {
+#             "children" => nil,
+#             "type" => 'file',
+#             "full_path" => full_path
+#           }
+
+#         end
+#       # else
+#       #   next
+#       # end
+#     end
+#     hash
+#   end
+
+# end
+
+# dir = ARGV[0].to_s
+# @t = FileTree.new(dir)
+# print @t.to_json
+
